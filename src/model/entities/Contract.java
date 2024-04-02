7c11c40a76c300be982edd4b0f5ec429ecddc177
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-02 19:22:52 -0300

    Creating contract

---
+++ src/model/entities/Contract.java
@@ -0,0 +1,46 @@
+package model.entities;
+
+import java.time.LocalDate;
+
+public class Contract {
+
+	private Integer number;
+	private LocalDate date;
+	private Double totalValue;
+	
+	private Installment installment;
+	
+	public Contract() { }
+
+	public Contract(Integer number, LocalDate date, Double totalValue) {
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(Double totalValue) {
+		this.totalValue = totalValue;
+	}
+	
+	
+}
